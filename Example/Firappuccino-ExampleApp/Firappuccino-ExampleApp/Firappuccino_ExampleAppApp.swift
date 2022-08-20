581954f6c10b4d001be95abb12ab69d779b7346d
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2022-08-19 20:07:32 -0500

    adds example app skeleton

---
+++ Example/Firappuccino-ExampleApp/Firappuccino-ExampleApp/Firappuccino_ExampleAppApp.swift
@@ -0,0 +1,15 @@
+//  Firappuccino-ExampleApp
+//
+//  Created by joshua kaunert on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Firappuccino_ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
