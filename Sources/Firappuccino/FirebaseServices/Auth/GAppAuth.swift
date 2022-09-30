4eeaaa6b20eb962d9520e4bc511cce8ac48f2c4f
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2022-09-30 15:04:23 -0500

    removes import statement

--- Sources/Firappuccino/FirebaseServices/Auth/GAppAuth.swift
+++ Sources/Firappuccino/FirebaseServices/Auth/GAppAuth.swift
@@ -1,7 +1,7 @@
 @_exported import AppAuth
 @_exported import GTMAppAuth
 
-import UIKit
+//import UIKit
 /// Wrapper class that provides convenient AppAuth functionality with Google Services.
 /// Set `ClientId`, `RedirectUri` and call respective methods where you need them.
 /// Requires dependency to GTMAppAuth, see: https://github.com/google/GTMAppAuth
